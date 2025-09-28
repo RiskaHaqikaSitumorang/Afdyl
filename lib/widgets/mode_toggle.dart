d3b8498e9188e05588206d7e9dd09571d9dbadd4
glennhkm <glenn@mhs.usk.ac.id>
2025-09-29 01:25:44 +0700

    feat: design improvement

--- lib/widgets/mode_toggle.dart
+++ lib/widgets/mode_toggle.dart
@@ -10,7 +10,7 @@ class ModeToggle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 40),
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
       child: Row(
         children: [
           Expanded(
